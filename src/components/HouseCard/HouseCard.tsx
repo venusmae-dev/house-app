7aefdb88a3b655bc73d2524fc0279d03c75c71fb
venus_mae <veemaee@gmail.com>
2026-04-24 02:39:19 -0700

    add card to contain house details

---
+++ src/components/HouseCard/HouseCard.tsx
@@ -0,0 +1,29 @@
+import { House } from "@/api/houses";
+import styles from "./HouseCard.module.css";
+
+interface HouseCardProps {
+  house: House;
+  onFavorite: (house: House) => void;
+  isFavorited: boolean;
+}
+
+const HouseCard = ({ house, onFavorite, isFavorited }: HouseCardProps) => {
+  return (
+    <div className={styles.houseCard}>
+      <div className={styles.imageWrapper}>
+        <img src={house.photoURL} alt={house.address} className={styles.cardImage} />
+        <button className={styles.favoriteBtn} onClick={() => onFavorite(house)}>
+          {isFavorited ? "♥" : "♡"}
+        </button>
+      </div>
+      <div className={styles.cardDetails}>
+        <p className={styles.cardPrice}>{house.id}</p>
+        <p className={styles.cardPrice}>${house.price.toLocaleString()}</p>
+        <p className={styles.homeowner}>Owned by {house.homeowner}</p>
+        <p className={styles.address}>{house.address}</p>
+      </div>
+    </div>
+  );
+};
+
+export default HouseCard;
